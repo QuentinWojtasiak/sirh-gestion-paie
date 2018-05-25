89f30ea95a81582529c018c1b1acead4e142067f
quentinw <quent.wojtasiak@gmail.com>
2018-05-25 17:20:46 +0200

    tp3.3

--- src/main/java/dev/paie/config/ServicesConfig.java
+++ src/main/java/dev/paie/config/ServicesConfig.java
@@ -2,11 +2,14 @@ package dev.paie.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan({ "dev.paie.services", "dev.paie.util" })
-// @Import(JddConfig.class)
-// @ImportResource("classpath:jdd-config.xml")
+@Import({ JpaConfig.class, DataSourceMySQLConfig.class })
+
+@EnableJpaRepositories("dev.paie.repository") // config a ajouter
 public class ServicesConfig {
 
 }
--- src/main/java/dev/paie/entite/Avantage.java
+++ src/main/java/dev/paie/entite/Avantage.java
@@ -2,11 +2,22 @@ package dev.paie.entite;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Avantage {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
+	@Column(name = "code", length = 30, nullable = false)
 	private String code;
+	@Column(name = "nom", length = 30, nullable = false)
 	private String nom;
+	@Column(name = "montant", nullable = true)
 	private BigDecimal montant;
 
 	public String getCode() {
